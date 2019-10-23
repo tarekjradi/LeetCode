69619503420502c825ee11c0c353cc687f0bdbfd
TarekJradi <jradi3@gmail.com>
2019-10-24 00:51:38 +0100

    added Valid Parentheses function

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/*
 var str = "Hello, playground"
 
 /*
@@ -100,3 +101,57 @@ func isPalindrome(_ x: Int) -> Bool {
 }
 
 isPalindrome(121)
+
+/*
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ An input string is valid if:
+
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ Note that an empty string is also considered valid.
+ 
+ Example 1:
+
+ Input: "()"
+ Output: true
+ Example 2:
+
+ Input: "()[]{}"
+ Output: true
+ Example 3:
+
+ Input: "(]"
+ Output: false
+ Example 4:
+
+ Input: "([)]"
+ Output: false
+ Example 5:
+
+ Input: "{[]}"
+ Output: true
+ 
+*/
+
+ */
+
+func isValid(_ s: String) -> Bool {
+    var stack: [Character] = []
+    for bracket in s {
+        switch bracket {
+        case "(":
+            stack.append(")")
+        case "{":
+            stack.append("}")
+        case "[":
+            stack.append("]")
+        default:
+            guard bracket == stack.popLast() else { return false }
+        }
+    }
+    return stack.isEmpty
+}
+
+isValid("([]{})")
+
